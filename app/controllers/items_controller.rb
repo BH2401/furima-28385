044d0bfc81bab09db33c731055bcb0d504fe0cd0
BH2401 <zingigene@gmail.com>
2020-11-16 22:02:22 +0900

    バリデーションとMVC設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,9 +4,23 @@ class ItemsController < ApplicationController
     
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
-  def message_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,22 @@ class Item < ApplicationRecord
   belongs_to :postage_payer
   belongs_to :prefecture
   belongs_to :handling_time
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price,
+              format: { with: /\A[0-9]+\z/, message: "Half-width number"},
+              numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "Out of setting range"}
+    validates :image
+  end
+
+  with_options numericality: { other_than: 0 } do
+    validates :category_id, format: { message: "Select"} 
+    validates :condition_id, format: { message: "Select"} 
+    validates :postage_payer_id, format: { message: "Select"} 
+    validates :prefecture_id, format: { message: "Select"} 
+    validates :handling_time_id, format: { message: "Select"} 
+  end
+
 end
