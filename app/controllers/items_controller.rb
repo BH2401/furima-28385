d1f1d7c94131771e26d71a8422a4a16248d7d54e
BH2401 <zingigene@gmail.com>
2020-11-21 18:11:41 +0900

    editのルーティング、編集ボタン、コントローラーの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :set_item, only: [:show, :edit]
+
   def index
     @items = Item.includes(:user).order('created_at DESC')
   end
@@ -19,12 +21,18 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
