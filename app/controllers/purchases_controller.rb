cd4f2460b0ad6ef0e5a67dc4e9f418f90e740d07
BH2401 <zingigene@gmail.com>
2020-11-22 18:01:15 +0900

    購入画面に移行できるようにした

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,17 @@
 class PurchasesController < ApplicationController
+
+  def index
+  end
+
+  def create
+    @purchase = Purchase.create(purchase_params)
+    redirect_to item_purchases_path
+
+  end
+
+  private
+
+  def purchase_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
+  end
 end
