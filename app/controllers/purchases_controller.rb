ac790ee3f511d6eefc0ab843ab5896bb15d111dd
BH2401 <zingigene@gmail.com>
2020-11-22 21:03:01 +0900

    addressesテーブル作成

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,17 +1,25 @@
 class PurchasesController < ApplicationController
+  before_action :set_purchase, only: [:index]
 
   def index
+    
+  end
+
+  def new
+    @purchase = Purchase.new
   end
 
   def create
     @purchase = Purchase.create(purchase_params)
-    redirect_to item_purchases_path
-
   end
 
   private
 
   def purchase_params
-    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :postage_payer_id, :prefecture_id, :handling_time_id, :price).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def set_purchase
+    @purchase = Purchase.find(params[:item_id])
   end
 end
