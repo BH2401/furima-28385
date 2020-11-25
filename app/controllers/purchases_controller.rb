595e6ca9a9fac5a282fa1cea7b4d417cf39ddcba
BH2401 <zingigene@gmail.com>
2020-11-25 15:03:37 +0900

    トークンなどの情報送信と決済完了

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,7 @@ class PurchasesController < ApplicationController
     @purchase = ItemPurchase.new(purchase_params)
 
     if @purchase.valid?
+      pay_item
       @purchase.save
       return redirect_to root_path
     else
@@ -19,11 +20,19 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:item_purchase).permit(:post_code, :prefecture_id, :city, :area, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:item_purchase).permit(:post_code, :prefecture_id, :city, :area, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
   
   def set_purchase
-    # @purchase = Purchase.find(params[:item_id])
     @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_366bb3ae03141fde045c3548"  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],    # カードトークン
+      currency:'jpy'                 # 通貨の種類(日本円)
+    )
   end
 end
